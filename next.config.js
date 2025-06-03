f8b5172585af7f41747331c1e71c4dddd8be7c2a
GlobalBlockInc <hyside76@protonmail.com>
2025-06-02 17:08:00 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,40 @@
+// AIOS-frontend/next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  poweredByHeader: false,
+  images: {
+    domains: ['thriveomate.com', 'localhost', 'cdn.thriveomate.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;

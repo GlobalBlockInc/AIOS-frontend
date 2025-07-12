1aa739c18c6cf17c69ed4c4880f5e595cac7e7b6
GlobalBlockInc <hyside76@protonmail.com>
2025-07-12 13:48:14 -0700

    Update admin-auth.js

--- middleware/admin-auth.js
+++ middleware/admin-auth.js
@@ -1,5 +1,5 @@
 export default function adminAuth(req, res, next) {
-  const secret = process.env.NEXT_PUBLIC_ADMIN_PASSWORD;
+  const secret = process.env.NEXT_PUBLIC_ADMIN_SECRET;
   const userSecret = req.headers['x-admin-secret'];
 
   if (!userSecret || userSecret !== secret) {
